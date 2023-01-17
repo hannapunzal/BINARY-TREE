7e3763102e57b8208747c9839a3574e3d522e3e1
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 15:19:42 +0800

    Initialized reading for the exercise

---
+++ binaryfirstpartexercise.py
@@ -0,0 +1,13 @@
+# find min
+# find max
+# calculate sum
+# in order traversal
+# pre order traversal
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    
