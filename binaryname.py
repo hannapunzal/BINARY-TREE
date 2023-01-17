91d60495e12b26199d4a163f265532959c4aa9d8
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 20:09:53 +0800

    Initialized reading and adding subtree

---
+++ binaryname.py
@@ -0,0 +1,23 @@
+# Use the letters in your fullname as the content of the binary tree
+
+class BinarySearchTreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return #existent node
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+        
