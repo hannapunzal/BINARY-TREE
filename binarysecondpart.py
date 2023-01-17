caaaff7bec9df114e7a490ae826a82b5818c2565
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 17:50:34 +0800

    Created function for adding sub

--- binarysecondpart.py
+++ binarysecondpart.py
@@ -3,4 +3,18 @@ class BinarySearchTreeNode:
         self.data = data
         self.left = None
         self.right = None
+
+    def add_child(self,data):
+        if data == self.data:
+            return #existent node
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode()
         
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode()
