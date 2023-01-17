05bfe1d81797ac62c3492537910e10c1187cb710
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 09:14:07 +0800

    Finished structuring the first traversal for binary search

--- binaryfirstpart.py
+++ binaryfirstpart.py
@@ -28,4 +28,6 @@ class BinarySearchTreeNode:
         elements.append(self.data)
 
         if self.right:
-            
+            elements += self.right.in_order_traversal()
+        
+        return elements
