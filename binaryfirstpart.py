68f976775b09c816d4d366880e757a64ee501a75
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 09:12:58 +0800

    Building codes through classes for the exercise

---
+++ binaryfirstpart.py
@@ -0,0 +1,31 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def add_child(self,data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def in_order_traversal(self):
+        elements =[]
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            
