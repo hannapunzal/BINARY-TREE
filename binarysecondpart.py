c47dd2b755e4c96e3481ef6a2f244a24a046b58b
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 18:01:37 +0800

    Added delete, finding max element and min element in the binary tree

--- binarysecondpart.py
+++ binarysecondpart.py
@@ -48,4 +48,35 @@ class BinarySearchTreeNode:
             else:
                 return False
 
+
+    def find_min(self):
+        if self.left is None:
+            return self.data
+        return self.left.find_min()
+    
+    def find_max(self):
+        if self.right is None:
+            return self.data
+        return self.right.find_min()
+
+    def delete(self, val):
+        if val < self.data:
+            if self.left:
+                self.left = self.left.delete(val)
+        elif val > self.data:
+            if self.right:
+                self.right = self.right.delete(val)
+        else:
+            if self.left is None and self.right is None:
+                return None
+            elif self.left is None:
+                return self.right
+            elif self.right is None:
+                return self.left
+
+            min_val = self.right.find_min()
+            self.data = min_val
+            self.right = min_val
+        
+        return self
     
