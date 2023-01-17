846e74ba87498ba5e0035b2691b48d5f61189378
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 09:17:45 +0800

    Implemented searching function

--- binaryfirstpart.py
+++ binaryfirstpart.py
@@ -31,3 +31,21 @@ class BinarySearchTreeNode:
             elements += self.right.in_order_traversal()
         
         return elements
+
+    def search(self, val):
+        if self.data == val:
+            return True
+        
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+        
