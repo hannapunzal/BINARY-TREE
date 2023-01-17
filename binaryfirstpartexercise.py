b36ab27933a66dbd59b4a0f76bc339f3445d9d68
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-01-17 15:22:27 +0800

    Implemented adding child function

--- binaryfirstpartexercise.py
+++ binaryfirstpartexercise.py
@@ -10,4 +10,18 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-    
+    def join_child(self, data):
+        if data == self.data:
+            return # for existent node
+        
+        if data < self.data:
+            if self.left:
+                self.left.join_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        
+        else:
+            if self.right:
+                self.right.join_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
